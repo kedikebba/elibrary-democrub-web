97065524d90a5ed23cb9e8684245c680a8e6f2f5
kedikebba <kebbakedi@gmail.com>
2019-08-15 12:58:57 -0500

    project complete

---
+++ src/main/java/edu/mum/cs/cs425/demos/elibrarydemocrudweb/service/BookService.java
@@ -0,0 +1,14 @@
+package edu.mum.cs.cs425.demos.elibrarydemocrudweb.service;
+
+import edu.mum.cs.cs425.demos.elibrarydemocrudweb.model.Book;
+
+public interface BookService {
+
+    public abstract Iterable<Book> getAllBooks();
+    public abstract Book saveBook(Book book);
+    public abstract Book getBookById(Integer bookId);
+    public abstract void deleteBookById(Integer bookId);
+    public abstract Iterable<Book> searchBookBy(String name);
+    public abstract Iterable<Book> getAllSortedBooks(String s);
+
+}
